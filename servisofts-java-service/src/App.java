55da75f1f436e552d388ca08e99d9e5150818a6a
rickypazd <35882906+rickypazd@users.noreply.github.com>
2021-11-28 15:23:53 -0400

    sd

--- servisofts-java-service/src/App.java
+++ servisofts-java-service/src/App.java
@@ -3,12 +3,6 @@ import Servisofts.Servisofts;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        try {
-            Servisofts.ManejadorCliente = ManejadorCliente::onMessage;
-            Servisofts.Manejador = Manejador::onMessage;
-            Servisofts.initialize();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Servisofts.initialize();
     }
 }
